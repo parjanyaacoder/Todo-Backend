fbf125a5f728957779e03810f298e6312252bcab
parjanyaacoder <padityashukla@gmail.com>
2023-09-10 15:53:18 +0530

    Added GetTodo, UpdateTodo APIs

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,5 +1,14 @@
 const TodoModel = require("../models/todo")
 
+async function handleGetTodoById(req, res) {
+    console.log('req.params:', req.params);
+    const { id } = req.params || {};
+    console.log('id:', id);
+    const result = await TodoModel.findById(id);
+    console.log('result:', result);
+    return res.status(200).json(result);
+}
+
 async function handleCreateTodo(req, res) {
     const body = req?.body
     console.log("Parjanya",body, req, !body, !body?.title,!body?.description,!body?.date)
@@ -17,6 +26,23 @@ async function handleCreateTodo(req, res) {
     return res.status(200).json({message: "Success"})
 }
 
+async function handleUpdateTodo(req, res) {
+    const { id } = req.params || {};
+    const body = req?.body
+    if (!body || !body.title ||!body.description || !body.date) {
+        return res.status(400).json({error: "Missing required fields"})
+    }
+    const result = await TodoModel.findByIdAndUpdate(id, {
+        title: body.title,
+        description: body.description,
+        done: body.done,
+        lastReferencedAt: Date.now()
+    })
+    return res.status(200).json({message: "Success", todo: result})
+}
+
 module.exports = {
-  handleCreateTodo
+  handleCreateTodo,
+  handleGetTodoById,
+  handleUpdateTodo
 }
--- routes/todo.js
+++ routes/todo.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { handleCreateTodo } = require('../controllers/todo');
+const { handleCreateTodo, handleGetTodoById, handleUpdateTodo } = require('../controllers/todo');
 
+router.get("/getTodo/:id", handleGetTodoById)
 router.post("/createTodo", handleCreateTodo)
+router.put("/updateTodo/:id", handleUpdateTodo)
 
 module.exports = router;
